cde3d0d350ef7eb481e90c4d671defe454ff3800
Michael <michael_mosienko@mail.ru>
2023-07-08 18:25:43 +0200

    Added the method, in which the loop, commensurate with the length of the array

--- Program.cs
+++ Program.cs
@@ -9,3 +9,8 @@
 
 string[] array1 = new string[5] {"CHF", "555", "EU", "Euro", "USD"};
 string[] array2 = new string[array1.Length];
+
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+int count = 0;
+for (int i = 0; i < array1.Length; i++)
