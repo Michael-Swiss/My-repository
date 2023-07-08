edc75c427e3bdd9454930dd67337bb903cd59560
Michael <michael_mosienko@mail.ru>
2023-07-08 18:32:00 +0200

    Added the condition of length ( <=3 ) of second - array result

--- Program.cs
+++ Program.cs
@@ -14,3 +14,11 @@ void SecondArrayWithIF(string[] array1, string[] array2)
 {
 int count = 0;
 for (int i = 0; i < array1.Length; i++)
+{
+if(array1[i].Length <= 3)
+{
+array2[count] = array1[i];
+count++;
+}
+}
+}
