754538fdbca92b91fbb1b93af44e9b5a63ebe063
Michael <michael_mosienko@mail.ru>
2023-07-08 18:41:43 +0200

    Enumeration and output on display

--- Program.cs
+++ Program.cs
@@ -22,3 +22,13 @@ count++;
 }
 }
 }
+void PrintArray(string[] array)
+{
+for (int i = 0; i < array.Length; i++)
+{
+Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
